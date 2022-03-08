3818792200e1dd619422792c0f2869e2e2496649
hyunjuyo <hjunyoo17@gmail.com>
2022-03-08 17:31:42 +0900

    220308

--- 다이나믹 프로그래밍/2203_boj10942_팰린드롬.py
+++ 다이나믹 프로그래밍/2203_boj10942_팰린드롬.py
@@ -1,18 +1,12 @@
+import sys
+
 N = int(input())
-
-result = [0] * (N+1)
-minus = [0] * (N+1)
-for i in range(1, N+1):
-    if i == 1:
-        result[i] = 3
-        minus[i] = 2 # 맨 아래줄에 사자가 있는 경우의수
-        continue
-    elif i == 2:
-        result[i] = result[i-1] * 3 - minus[i-1]
-        minus[i] = minus[i-1] * 2
-        continue
-
-    result[i] = (result[i-1] * 3 - minus[i-1]) % 9901
-    minus[i] = minus[i-1] * 2 + 2
-
-print(result)
+NUM = list(map(int, input().split()))
+M = int(input())
+for _ in range(M):
+    S, E = map(int, sys.stdin.readline().split())
+    num = NUM[S-1:E]
+    if num == num[::-1]:
+        print(1)
+    else:
+        print(0)
