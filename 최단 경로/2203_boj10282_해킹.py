5b5fef00ab67141d762814030c2d1bd3f0737999
hyunjuyo <hjunyoo17@gmail.com>
2022-03-10 15:51:18 +0900

    220310

--- 최단 경로/2203_boj10282_해킹.py
+++ 최단 경로/2203_boj10282_해킹.py
@@ -2,8 +2,8 @@ import sys
 import heapq
 
 def dijkstra(num):
-    check[num] = True
-    time[num] = 0
+    check[num] = True # 감염여부
+    time[num] = 0 # 시간정보
     h = []
     heapq.heappush(h, (0, num))
     while h:
@@ -25,19 +25,19 @@ for _ in range(T):
         A, B, S = map(int, sys.stdin.readline().split())
         info[B].append((S, A))
     
-    check = [False] * (N+1)
-    time = [1e9] * (N+1)
+    check = [False] * (N+1) # 컴퓨터별 감염여부 저장
+    time = [1e9] * (N+1) # 컴퓨터별 최소 도달시간 저장
     dijkstra(C)
+    # print('check :', check) # test
+    # print('time :', time) # test
 
-    print('check :', check) # test
-    print('time :', time) # test
-
+    # 감염된 컴퓨터 개수 및 감염된 컴퓨터 중 가장 긴 시간 정보 저장
     result_count = 0
     result_time = 0
     for i in range(N+1):
         if check[i] == True:
-            result_count += 1
-            if result_time < time[i]:
+            result_count += 1 # 감염된 컴퓨터 개수 +1
+            if result_time < time[i]: # 가장 긴 시간 정보로 업데이트
                 result_time = time[i]
 
     print(result_count, result_time)
