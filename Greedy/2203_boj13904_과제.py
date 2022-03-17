632d3780906ce53638574c900537058191cd217f
hyunjuyo <hjunyoo17@gmail.com>
2022-03-17 12:32:21 +0900

    220317

---
+++ Greedy/2203_boj13904_과제.py
@@ -0,0 +1,34 @@
+
+
+N = int(input())
+# arr = []
+max_day = 0
+info = [list() for _ in range(N+1)]
+for _ in range(N):
+    D, W = map(int, input().split())
+    info[D].append(W)
+    max_day = max(max_day, D)
+    # arr.append((D, W))
+
+# arr.sort(key=lambda x:(x[0], -x[1]))
+# print(arr)
+
+# arr.sort(key=lambda x:(-x[1], x[0]))
+# print(arr)
+
+print(info)
+
+result = []
+count = 0
+tmp = []
+for d in range(1, max_day+1):
+    count += len(info[d])
+    tmp.extend(info[d])
+    if count > d:
+        tmp.sort(reverse=True)
+        result.extend(tmp[:d])
+        count = d
+        tmp = [-1] * d
+
+print(result) # test
+print(sum(result))
