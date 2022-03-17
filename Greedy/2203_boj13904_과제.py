65aca30fff08cf37dc134c874665a349cb866857
hyunjuyo <hjunyoo17@gmail.com>
2022-03-17 14:53:21 +0900

    220317

--- Greedy/2203_boj13904_과제.py
+++ Greedy/2203_boj13904_과제.py
@@ -1,35 +1,28 @@
-
-
 N = int(input())
-# arr = []
 max_day = 0
-info = [list() for _ in range(N+1)]
+info = [list() for _ in range(1000+1)]
 for _ in range(N):
     D, W = map(int, input().split())
     info[D].append(W)
     max_day = max(max_day, D)
-    # arr.append((D, W))
 
-# arr.sort(key=lambda x:(x[0], -x[1]))
-# print(arr)
-
-# arr.sort(key=lambda x:(-x[1], x[0]))
-# print(arr)
-
-print(info)
+# print(info) # test
 
 result = []
-count = 0
-tmp = []
+tmp = [] # 임시 저장 리스트
+minus = 0
 for d in range(1, max_day+1):
-    # count += len(info[d])
-    tmp.extend(info[d])
-    if len(tmp) > d:
+    print('-'*20, 'd :', d) # test
+    tmp.extend(info[d]) # 임시 저장
+    print('tmp :', tmp) # test
+    if len(tmp) > d: # 일수보다 과제수가 더 많아질 경우
         tmp.sort(reverse=True)
-        result.extend(tmp[:d])
-        # count = d
-        tmp = [-1] * d
+        result.extend(tmp[:d - minus]) # 과제 점수 높은 순서로 결과 저장
+        tmp = [0] * d # 임시 저장 리스트에 개수만 채우기
+        minus = d
+        print('result :', result) # test
 
+# 임시 저장 리스트에 남아있는 과제 점수 더하기
 tmp.sort(reverse=True)
 for v in tmp:
     if v > 0:
