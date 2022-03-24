ae518a2d67ab4392da3a7f53ee7c8399e15890ed
hyunjuyo <hjunyoo17@gmail.com>
2022-03-24 18:45:27 +0900

    220324

--- 구현/2203_boj16234_인구이동_test.py
+++ 구현/2203_boj16234_인구이동_test.py
@@ -1,5 +1,4 @@
 import sys
-from collections import deque
 sys.setrecursionlimit(100000)
 
 def check_boundary(table):
@@ -23,28 +22,19 @@ def dfs(y, x):
     global pos
     pos.append((y, x))
     tmp[y][x] = False
-    for (ny, nx) in info[y][x]:
+    for i in range(4):
+        ny = y + dir[i][0]
+        nx = x + dir[i][1]
+        if ny < 0 or ny >= N or nx < 0 or nx >= N:
+            continue
         if tmp[ny][nx]:
             dfs(ny, nx)
 
-# def bfs(y, x):
-#     global pos
-#     q = deque([(y, x)])
-#     while q:
-#         py, px = q.popleft()
-#         if tmp[py][px]: # 중복 방지
-#             pos.append((py, px))
-#         tmp[py][px] = False
-#         for (ny, nx) in info[py][px]:
-#             if tmp[ny][nx]:
-#                 q.append((ny, nx))
-
 def fill_new_num(pos):
     total_sum = 0
-    count = 0
+    count = len(pos)
     for i, j in pos:
         total_sum += table[i][j]
-        count += 1
     new = total_sum // count
     for i, j in pos:
         table[i][j] = new
@@ -56,13 +46,7 @@ def open_n_move(table):
             if tmp[i][j]:
                 pos = []
                 dfs(i, j)
-                print('-'*30) # test
-                print('pos :', pos) # test
-                for v in tmp: # test
-                    print(v)
                 fill_new_num(pos)
-                # for i, j in pos:
-                #     table[i][j] = new_num
 
 N, L, R = map(int, input().split())
 table = []
@@ -71,20 +55,16 @@ for _ in range(N):
 
 dir = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 result = 0
-tmp = [[False] * N for _ in range(N)]
 while True:
     pos = []
-    # tmp = [[False] * N for _ in range(N)]
+    tmp = [[False] * N for _ in range(N)]
     info = [[list() for _ in range(N)] for _ in range(N)]
     if check_boundary(table) == 0:
         break
-    print('here') # test
-    for v in tmp: # test
-        print(v)
-    print(pos) # test
+    # print(pos) # test
     open_n_move(table)
-    for v in table: # test
-        print(v)
+    # for v in table: # test
+    #     print(v)
     result += 1
 
 print(result)
