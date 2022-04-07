11986b2c9ec2e5ce3bd23c69cb023f22108134cb
hyunjuyo <hjunyoo17@gmail.com>
2022-04-07 17:38:16 +0900

    220407

--- 다이나믹 프로그래밍/2203_boj1516_게임개발.py
+++ 다이나믹 프로그래밍/2203_boj1516_게임개발.py
@@ -3,17 +3,18 @@ from collections import deque
 N = int(input())
 inDegree = [0] * (N+1)
 time = [0] * (N+1)
-info = [list() for _ in range(N+1)]
+l = [list() for _ in range(N+1)]
+r = [list() for _ in range(N+1)]
 for i in range(1, N+1):
     tmp = list(map(int, input().split()[:-1]))
     time[i] = tmp.pop(0)
     for v in tmp:
         inDegree[i] += 1
-        info[v].append(i)
+        l[i].append(v)
+        r[v].append(i)
 
 print("inDegree :", inDegree)
 print('time :', time)
-print('info :', info)
 
 result = [0] * (N+1)
 q = deque([])
@@ -23,12 +24,14 @@ for i in range(1, N+1):
 while q:
     now = q.popleft()
     result[now] += time[now]
-    for v in info[now]:
+    for v in r[now]:
         inDegree[v] -= 1
+        result[v] += result[now]
         if inDegree[v] == 0:
             q.append(v)
 
 print("inDegree :", inDegree)
 print('time :', time)
-print('info :', info)
+print(l)
+print(r)
 print(result)
