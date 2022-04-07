f2406ef5778077c0f67e47705befd0eefb61a153
hyunjuyo <hjunyoo17@gmail.com>
2022-04-07 23:30:53 +0900

    220407

--- test2.py
+++ test2.py
@@ -1,1 +1,34 @@
+import sys
+import heapq
 
+def dijkstra(s, k_num):
+    h = []
+    heapq.heappush(h, (0, s))
+    while h:
+        ptime, now = heapq.heappop(h)
+        if ptime > time_list[now][k_num]:
+            continue
+        for next, time in info[now]:
+            ntime = ptime + time
+            if ntime < time_list[next][k_num]:
+                if ntime not in time_list[next][1:k_num]:
+                    time_list[next][k_num] = ntime
+                heapq.heappush(h, (ntime, next))
+
+N, M, K = map(int, input().split())
+
+info = [list() for _ in range(N+1)]
+for _ in range(M):
+    a, b, c = map(int, sys.stdin.readline().split())
+    info[a].append((b, c))
+
+time_list = [[int(1e9)] * (K+1) for _ in range(N+1)]
+for k_num in range(1, K+1):
+    time_list[1][1] = 0
+    dijkstra(1, k_num)
+
+for row in time_list[1:]:
+    if row[K-1] == row[K] or row[K] == int(1e9):
+        print(-1)
+    else:
+        print(row[K])
--- 최단 경로/2203_boj1854_K번째최단경로찾기.py
+++ 최단 경로/2203_boj1854_K번째최단경로찾기.py
@@ -3,7 +3,6 @@ import heapq
 
 def dijkstra(s, k_num):
     h = []
-    time_list[s][k_num] = 0
     heapq.heappush(h, (0, s))
     while h:
         ptime, now = heapq.heappop(h)
@@ -23,17 +22,19 @@ for _ in range(M):
     a, b, c = map(int, sys.stdin.readline().split())
     info[a].append((b, c))
 
-print(info) # test
+print('info :', info) # test
 
 time_list = [[int(1e9)] * (K+1) for _ in range(N+1)]
+time_list[1][1] = 0 # 시작지점 1에 대해 첫번째 경우만 0으로 설정
 for k_num in range(1, K+1):
     dijkstra(1, k_num)
 
-for v in time_list: # test
-    print(v)
+print('time_list', '-'*70)
+for v in time_list[1:]: # test
+    print(v[1:])
 
 for row in time_list[1:]:
-    if row[K-1] == row[K]: # K번째 최단경로 존재하지 않는 경우
+    if row[K] == int(1e9): # K번째 최단경로 존재하지 않는 경우
         print(-1)
     else:
         print(row[K])
