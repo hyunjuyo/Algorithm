bb0f882c141a6b497299dfd7fffacff18f49235d
hyunjuyo <hjunyoo17@gmail.com>
2022-04-14 15:09:48 +0900

    220414

--- test2.py
+++ test2.py
@@ -1,47 +1,10 @@
-# import sys
-# import math
-
-# print(sys.maxsize)
-# print(sys.maxsize + 1111111111111111111)
-
-# print(math.inf)
-# print(math.inf + 1111111111111111111)
-
-# print(math.isinf)
-
-from collections import defaultdict, deque
 import sys
+import math
 
-input = sys.stdin.readline
+print(sys.maxsize)
+print(sys.maxsize + 1111111111111111111)
 
-N = int(input())
+print(math.inf)
+print(math.inf + 1111111111111111111)
 
-dp_table = [0] * (N + 1)
-degree = [0] * (N + 1)
-dependency = defaultdict(list)
-work_time = [0] * (N + 1)
-
-for i in range(1, N + 1):
-    S = list(map(int, input().split()))
-
-    work_time[i] = S[0]
-    degree[i] = S[1]
-    for element in S[2:]:
-        dependency[element].append(i)
-
-Q = deque()
-
-for i in range(1, len(degree)):
-    if degree[i] == 0:
-        Q.append(i)
-        dp_table[i] = work_time[i]
-
-while Q:
-    now = Q.popleft()
-    for element in dependency[now]:
-        degree[element] -= 1
-        dp_table[element] = max(dp_table[element], dp_table[now] + work_time[element])
-        if degree[element] == 0:
-            Q.append(element)
-
-print(max(dp_table))
+print(math.isinf)
