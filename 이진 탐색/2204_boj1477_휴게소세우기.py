8ae640dbecea894e798bcc9cbf70ceadf6bfa274
hyunjuyo <hjunyoo17@gmail.com>
2022-04-24 17:00:53 +0900

    220424

--- 이진 탐색/2204_boj1477_휴게소세우기.py
+++ 이진 탐색/2204_boj1477_휴게소세우기.py
@@ -13,17 +13,42 @@ def get_max_length(sorted_arr):
         idx = L
     return max_length, idx
 
+def add_spots(arr, mid):
+    tmp_arr = arr[:]
+    tmp_arr.insert(0, 0)
+    tmp_arr.append(L)
+    count = 0
+    i = 0
+    while True:
+        i += 1
+        if i == len(tmp_arr):
+            break
+        if tmp_arr[i] - tmp_arr[i-1] > mid:
+            tmp_arr.insert(i, tmp_arr[i-1] + mid)
+            count += 1
+    tmp_arr.pop(0)
+    tmp_arr.pop()
+    return tmp_arr, count
+
 N, M, L = map(int, input().split())
 pos = list(map(int, input().split()))
 pos.sort()
 
-for n in range(1, M+1):
-    print(n, '-'*50) # test
-    max_len, idx = get_max_length(pos)
-    print('max_len, idx :', max_len, idx) # test
-    new_len = max_len // 2
-    pos.insert(idx, pos[idx] - new_len)
-    print(pos) # test
-
+result = INF
 max_len, idx = get_max_length(pos)
-print(max_len)
+l = 1
+r = max_len
+while l <= r:
+    mid = (l + r) // 2
+    print('mid :', mid, '-'*50) # test
+
+    tmp_arr, count = add_spots(pos, mid)
+    print(tmp_arr, count) # test
+
+    if count <= M:
+        result = min(result, mid)
+        r = mid - 1
+    else:
+        l = mid + 1
+
+print(result)
