362e96f102b0d5aad207c4a0601bb3e2742ff011
hyunjuyo <hjunyoo17@gmail.com>
2022-04-24 14:34:57 +0900

    220424

--- test2.py
+++ test2.py
@@ -1,10 +1,17 @@
-import sys
-import math
+# import sys
+# import math
 
-print(sys.maxsize)
-print(sys.maxsize + 1111111111111111111)
+# print(sys.maxsize)
+# print(sys.maxsize + 1111111111111111111)
 
-print(math.inf)
-print(math.inf + 1111111111111111111)
+# print(math.inf)
+# print(math.inf + 1111111111111111111)
 
-print(math.isinf)
+# print(math.isinf)
+
+arr = [1,2,3,4,5]
+
+arr.insert(5, 7)
+print(arr)
+arr.insert(6, 9)
+print(arr)
